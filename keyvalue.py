2179d51ea868c0b3aebd319218c2c2e511e7678f
Bharath A <bbharath201@gmail.com>
2020-12-28 16:32:52 +0530

    keyValue functions completed

--- keyvalue.py
+++ keyvalue.py
@@ -1,16 +1,40 @@
-from pymongo import MongoClient
+import pymongo
+import sys
 class CRD:
-    def __init__(self):
-        self.client = MongoClient()
-        self.dataBase = client['keyValue']
-        self.collection= dataBase['keyValue'] 
+    def __init__(self,dataBase,collection):
+        self.client = pymongo.MongoClient()
+        self.dataBase = self.client[dataBase]
+        self.collection= self.dataBase[collection] 
 
-    def insert(key,value):
-        pass
-    def update(key,value):
-        pass
-    def delete(key):
-        pass
+    def insert(self,key,value,ttl=None):
+        
+        if (len(str(key))>32):
+            print("The maximum key length is 32.")
+            return False
+        if (sys.getsizeof(value)>16000):
+            print("The value size must be less than 16KB.")
+            return False
+        try:
+            self.collection.insert_one({"id":key,"value":value})
+        except pymongo.errors.DuplicateKeyError:
+            print("This Key already exists")
+
+    def read(self,key):
+        try:
+            x = self.collection.find({"id":key},{"value":1})
+            print(list(x)[0]['value'])
+        except IndexError:
+            print('No such key in database.')
+
+    def delete(self,key):
+        result = self.collection.delete_many({"id":key})
+        if(result.deleted_count==0):
+            print("No such element exists.")
+        else:
+            print("Item deleted successfully")
 
 if __name__ =='__main__':
-    pass
+    test = CRD('keyValue','keyValue')
+    # test.insert(5,"{'test':'success'}")
+    test.delete(1)
+    # test.read(6)
--- test_keyvalue.py
+++ test_keyvalue.py
@@ -1,6 +1,23 @@
+import unittest
+import keyvalue
 from pymongo import MongoClient
 
+class TestKeyValue(unittest.TestCase):
+
+    def test_connectivity(self):
+        self.testClass = keyvalue.CRD('keyValue','keyValue')
+
+    def test_ifduplicate(self):
+        self.testClass.insert(1,"test")
+        self.assertEquals(self.testClass.insert(1,"test"),"This Key already exists")
+
+    def test_ifValueAdding(self):
+        pass
+    def test_ifDeletion(self):
+        pass
+    def test_nullDeletion(self):
+        pass
 if __name__ =='__main__':
-    client = MongoClient()
-    dataBase = client['keyValue']
-    collection= dataBase['keyValue'] 
+    test = TestKeyValue()
+    test.test_connectivity()
+    test.test_ifduplicate()
